a977ed76c5c000f39aa243deb3c3491b45497377
Dan Davies <dandavies.bagillt@googlemail.com>
2013-03-23 10:33:30 +0000

    Added new home content
    Signed-off-by: Dan Davies <dandavies.bagillt@googlemail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,34 @@
+// Adding an active class to a menu item
 
+function scriptInit() {
+if (!document.getElementById) {
+	return;
+	}
+}
+function addEvent(elm, evType, fn, useCapture) {
+	if (elm.addEventListener) {
+	elm.addEventListener(evType, fn, useCapture);
+	return true;
+	} else if (elm.attachEvent) {
+	var r = elm.attachEvent('on' + evType, fn);
+	return r;
+	} else {
+	elm['on' + evType] = fn;
+	}
+}
+function checkActive() {
+	var a = document.getElementsByTagName("a");
+	if (window.location.href.substr(location.href.length - 1, 1) == '/') {
+		var loc = window.location.href + 'index.html'; 
+	}
+	else {
+		var loc = window.location.href;
+	}
+	for(var i=0; i < a.length; i++) {
+		if (a[i].href == loc) {
+			a[i].setAttribute("class", "active");
+			a[i].setAttribute("className", "active");
+		}
+	}
+}
+addEvent(window, 'load', checkActive, false);
